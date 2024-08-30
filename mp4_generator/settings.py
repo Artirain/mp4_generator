cd9c0b49aec56b8473e10c139b55588c241ac1f0
Astoriy <ansre1313@gmail.com>
2024-08-30 17:15:31 +0600

    database changes, changed postgresql to sqlite3

--- mp4_generator/settings.py
+++ mp4_generator/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mp4_generator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mp4_generator', 
-        'USER': 'postgres',  
-        'PASSWORD': '203599art1313',  
-        'HOST': 'localhost',  
-        'PORT': '5432',  
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
